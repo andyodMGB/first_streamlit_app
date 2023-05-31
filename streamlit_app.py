50d30e52f94d3fbef54bb10e078150c472fb09b5
andyodMGB <134540927+andyodMGB@users.noreply.github.com>
2023-05-31 19:26:55 -0400

    Update streamlit_app.py
    moved python imports to top of code; added URL Error library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError 
+
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -10,7 +15,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+# import pandas moved to top
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,7 +31,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+# import requests moved to top
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # normalize the json raw data
@@ -37,8 +42,7 @@ streamlit.dataframe(fruityvice_normalized)
 ##troubleshooting
 streamlit.stop()
 
-import snowflake.connector
-
+#import snowflake.connector moved to top
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
